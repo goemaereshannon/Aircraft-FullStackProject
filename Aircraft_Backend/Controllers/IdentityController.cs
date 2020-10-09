a995fe003776f8f1d27e1ef478e877ba5a083d16
Alexandra <alexandra.christiaens@student.howest.be>
2020-10-09 18:34:23 +0200

    configure services + add identitycontroller

---
+++ Aircraft_Backend/Controllers/IdentityController.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Aircraft.Models.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
+
+namespace Aircraft.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class IdentityController : ControllerBase
+    {
+        private readonly AircraftDbContext _context;
+        private readonly SignInManager<Person> signInManager;
+        private readonly IPasswordHasher<Person> hasher;
+        private readonly UserManager<Person> personManager;
+
+        public IdentityController(AircraftDbContext context, SignInManager<Person> signInManager, IConfiguration configuration, IPasswordHasher<Person> hasher, UserManager<Person> PersonManager, GenericRepo<Person> repo)
+        {
+            _context = context;
+            this.signInManager = signInManager;
+            this.hasher = hasher;
+            personManager = PersonManager;
+        }
+
+        // GET: api/Identity
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Person>>> GetPersons()
+        {
+            return await _context.Persons.ToListAsync();
+        }
+
+        // GET: api/Identity/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Person>> GetPerson(Guid id)
+        {
+            var person = await _context.Persons.FindAsync(id);
+
+            if (person == null)
+            {
+                return NotFound();
+            }
+
+            return person;
+        }
+
+        // PUT: api/Identity/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutPerson(Guid id, Person person)
+        {
+            if (id != person.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(person).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!PersonExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Identity
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPost]
+        public async Task<ActionResult<Person>> PostPerson(Person person)
+        {
+            _context.Persons.Add(person);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetPerson", new { id = person.Id }, person);
+        }
+
+        // DELETE: api/Identity/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Person>> DeletePerson(Guid id)
+        {
+            var person = await _context.Persons.FindAsync(id);
+            if (person == null)
+            {
+                return NotFound();
+            }
+
+            _context.Persons.Remove(person);
+            await _context.SaveChangesAsync();
+
+            return person;
+        }
+
+        private bool PersonExists(Guid id)
+        {
+            return _context.Persons.Any(e => e.Id == id);
+        }
+    }
+}
