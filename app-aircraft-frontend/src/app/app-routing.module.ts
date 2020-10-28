fb45bac802477eff84cc4356797ce8757729b22b
Unknown <shannon.goemaere@student.howest.be>
2020-10-28 16:42:11 +0100

    Basic structure

--- app-aircraft-frontend/src/app/app-routing.module.ts
+++ app-aircraft-frontend/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // { path: 'first-component', component: FirstComponent },
+  // { path: 'second-component', component: SecondComponent },
+  // { path: '**', component: PageNotFoundComponent },  // Wildcard route for a 404 page
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
