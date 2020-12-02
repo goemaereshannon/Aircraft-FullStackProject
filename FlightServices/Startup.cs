30ec05d5e367df8c1aa2b27327c420179e99daa7
Shannon <shannon.goemaere@student.howest.be>
2020-12-02 21:32:55 +0100

    Installed update of angular/core package after merge

--- FlightServices/Startup.cs
+++ FlightServices/Startup.cs
@@ -71,7 +71,7 @@ namespace FlightServices
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            app.UseCors("MyAllowOrigins"); 
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -82,7 +82,11 @@ namespace FlightServices
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+
             app.UseHttpsRedirection();
+
+            app.UseCors("MyAllowOrigins");
+
             app.UseStaticFiles();
 
             app.UseRouting();
