44df93f1bcec892c0dff4fadc08af9725aba1669
Alexandra <alexandra.christiaens@student.howest.be>
2020-12-13 14:41:43 +0100

    small debugs flightcontroller

--- FlightServices/DTOs/PriceClassDTO.cs
+++ FlightServices/DTOs/PriceClassDTO.cs
@@ -11,6 +11,7 @@ namespace FlightServices.DTOs
         public DateTime BeginDate { get; set; }
         public DateTime EndDate { get; set; }
         public double Value { get; set; }
+        public string Currency { get; set; }
         //TODO: add currency object
 
         // public Guid? ClassId { get; set; } = Guid.Parse("3F2504E0-4F89-11D3-9A0C-0305E82C3301");
